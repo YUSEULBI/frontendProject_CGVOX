535660d3a5dd4835288b38d8edc9f9de2931f9e1
YUSEULBI <ha258ppy@gmail.com>
2023-05-12 12:03:34 +0900

    05/12

---
+++ TeamProject1-CGV/public/js/movie_view.js
@@ -0,0 +1,263 @@
+let movieList=[
+	{name:'아바타-물의 길',		img:'아바타.jpg',			감독 :'James Cameron'   , 장르 : 'SF', 		평점:8.83, 개봉일:'2022.12.14', 상영시간: 120, egg: '99', 예매율: '32.1'},
+	{name:'BTS: Yet To Come in Cinemas',img:'방탄콘서트.jpg',감독 :'오윤동' 	   , 장르 : '공연실황', 	평점:8.83, 개봉일:'2023.02.01', 상영시간: 120, egg: '99', 예매율: '32.1'},	
+	{name:'오늘 밤,세계에서 이 사랑이 사라진다해도',	img:'오늘밤사라진다해도.jpg',	감독 :'Takahiro Miki' , 장르 : 'SF', 	평점:8.83, 개봉일:'2022.12.14', 상영시간: 120, egg: '99', 예매율: '32.1'},
+	{name:'영웅',				img:'영웅.jpg',			감독 :'윤제균' 		   , 장르 : '드라마', 		평점:8.42, 개봉일:'2022.12.21', 상영시간: 120, egg: '99', 예매율: '32.1'},
+	{name:'교섭',				img:'교섭.jpg',			감독 :'임순례' 		   , 장르 : '드라마', 		평점:6.29, 개봉일:'2023.01.18', 상영시간: 120, egg: '99', 예매율: '32.1'},
+	{name:'상견니',			img:'상견니.jpg',			감독 :'Tianren Huang'   , 장르 : '드라마', 		평점:7.85, 개봉일:'2023.01.25', 상영시간: 120, egg: '99', 예매율: '32.1'},
+	{name:'올빼미',			img:'올빼미.jpg',			감독 :'안태진' 		   , 장르 : '스릴러', 		평점:8.73, 개봉일:'2022.11.13', 상영시간: 120, egg: '99', 예매율: '32.1'},
+	{name:'메간',				img:'메간.jpg',			감독 :'Gerard Johnstone', 장르 : '공포', 		평점:7.34, 개봉일:'2023.01.25', 상영시간: 120, egg: '99', 예매율: '32.1'},
+	{name:'슬램덩크',			img:'슬램덩크.jpg',		감독 :'Takehiko Inoue', 장르 : '애니메이션',		평점:9.24, 개봉일:'2023.01.04', 상영시간: 120, egg: '99', 예매율: '32.1'},
+	{name:'돌핀보이',			img:'돌핀보이.jpg',		감독 :'Andrey Goglev' , 장르 : '애니메이션', 		평점:6.85, 개봉일:'2023.01.25', 상영시간: 120, egg: '99', 예매율: '32.1'},
+	{name:'몬스터하우스',		img:'몬스터하우스.jpg',		감독 :'James Cameron' , 장르 : 'SF', 			평점:8.83, 개봉일:'2022.12.14', 상영시간: 120, egg: '99', 예매율: '32.1'},
+	{name:'장화신은 고양이:끝내주는 모험',img:'장화신은고양이.jpg',	감독 :'Joel Crawford' , 장르 : '애니메이션',	평점:8.41, 개봉일:'2023.01.04', 상영시간: 120, egg: '99', 예매율: '32.1'},	
+]
+
+let spaceList = [
+{local:'서울', space: [{s:'강남'},{s:'강변'},{s:'건대입구'},{s:'구로'},{s:'대학로'},{s:'등촌'},{s:'명동'},{s:'미아'},{s:'송파'},{s:'신촌'}] },
+{local:'경기', space: [{s:'광교'},{s:'구리'},{s:'김포'},{s:'동수원'},{s: '동탄'},{s:'부천'},{s:'안산'},{s:'야탑'},{s:'의정부'},{s:'인천'},{s:'일산'},{s:'판교'}]},
+{local:'충청', space: [{s:'논산'},{s:'대전'},{s:'세종'},{s:'아산'},{s:'천안'},{s:'청주'}]},
+{local:'전라', space: [{s:'광양'},{s:'광주'},{s:'목포'},{s:'순천'},{s:'익산'},{s:'전주'},{s:'제주'}]},
+{local:'경상', space: [{s:'김해'},{s:'대구'},{s:'마산'},{s:'서면'},{s:'울산'},{s:'창원'}]}
+]
+let roomList = [
+{room: '3관'			, times: [{time:'09:20'},{time:'13:00'},{time:'14:40'},{time:'16:00'}]},	
+{room: '4관'			, times: [{time:'10:00'},{time:'15:30'},{time:'18:10'},{time:'21:00'}]},		
+{room: '5관'		 	, times: [{time:'11:00'},{time:'15:30'},{time:'16:00'},{time:'22:40'}]},	
+{room: '4DX관'	 	, times: [{time:'09:20'},{time:'13:00'},{time:'14:40'},{time:'16:00'}]},
+{room: 'SCREENX관'	, times: [{time:'10:00'},{time:'15:30'},{time:'18:10'},{time:'21:00'}]}	
+]
+
+
+//--영화 리스트 출력-----------------------------------------------------------------------------------------------------//
+
+function list(){
+	
+	let html = ``
+	let 영화 = document.querySelector('.movie').innerHTML; console.log(영화);
+	
+	movieList.forEach((o,i)=>{
+	
+	html += `<button type="button" class="list m_list" onclick="print(${i})"> ${o.name} </button>`;
+		
+	})
+
+	document.querySelector('.movie_list').innerHTML =html
+	local_print()
+}
+
+
+//---선택 영화 정보 출력---------------------------------------------------------------------------------------------------//
+
+print(0)
+function print(i){
+	
+	let html = ``
+	
+	let html2 =``
+	
+	let choosen =  movieList[i].name
+	
+	movieList.forEach((o2)=>{
+	
+	if(choosen==o2.name)
+	{
+	html += `<div class="row"> 
+					<div class="col-md-3"> 
+						<img class="main_img" src="img/${o2.img}" width="100%">
+					</div>				
+					<div class="col-md-9 information"> 
+						<div class="m_title" > 	  	  ${o2.name}  </div>
+						<div>
+							  <div class="m_score">  예매율 ${ o2.예매율 }%<span>|</sapn><span class="al2"> <img src="img/main_page/황금알.png"> ${ o2.egg }%</span></div>
+							  <div class="m_info">	 평점 	:<span class="percent"> ${o2.평점}</span>	 </div>
+							  <div class="m_info">   감독 	: 	${o2.감독}		</div>
+							  <div class="m_info">   장르		: 	${o2.장르}		</div>
+							  <div class="m_info">   개봉일	:	${o2.개봉일}		</div>
+							  <div class="m_info">   상영시간	:	${o2.상영시간}	분	</div>
+						</div>														
+					</div>
+				</div>	<!-- row e -->
+				`
+				
+				
+	html2 = `<img src="img/${o2.img}" width="80px;">`
+	}	
+	})// for e
+	
+	document.querySelector('.moviebox').innerHTML = html
+	document.querySelector('.img').innerHTML = html2
+	document.querySelector('.movie').innerHTML = document.querySelector('.m_title').innerHTML
+	
+	
+}// fun e
+//-----info 출력---------------------------------------------------------------------------------------------------//
+// 주요정보 클릭 이벤트
+document.querySelector('.info_print').addEventListener ('click',(e)=>{
+	document.querySelector('.example_info').style.display='block';
+	document.querySelector('.story').style.display='block';
+	document.querySelector('.trailer').style.display='block';
+	document.querySelector('.still').style.display='block';
+	document.querySelector('.rating').style.display='block';
+	document.querySelector('.time_table').style.display='none';
+})
+// 트레일러 클릭 이벤트
+document.querySelector('.trailer_print').addEventListener ('click',(e)=>{
+	document.querySelector('.example_info').style.display='block';
+	document.querySelector('.story').style.display='none';
+	document.querySelector('.trailer').style.display='block';
+	document.querySelector('.still').style.display='none';
+	document.querySelector('.rating').style.display='none';
+	document.querySelector('.time_table').style.display='none';
+})
+// 스틸컷 클릭 이벤트
+document.querySelector('.still_print').addEventListener ('click',(e)=>{
+	document.querySelector('.example_info').style.display='block';
+	document.querySelector('.story').style.display='none';
+	document.querySelector('.trailer').style.display='none';
+	document.querySelector('.still').style.display='block';
+	document.querySelector('.rating').style.display='none';
+	document.querySelector('.time_table').style.display='none';
+})
+// 평점/리뷰 클릭 이벤트
+document.querySelector('.rating_print').addEventListener ('click',(e)=>{
+	document.querySelector('.example_info').style.display='block';
+	document.querySelector('.story').style.display='none';
+	document.querySelector('.trailer').style.display='none';
+	document.querySelector('.still').style.display='none';
+	document.querySelector('.rating').style.display='block';
+	document.querySelector('.time_table').style.display='none';
+})
+// 평점 작성 버튼 클릭 이벤트
+document.querySelector('.grade').addEventListener ('click',(e)=>{
+	alert('로그인이 필요한 서비스입니다.\n로그인페이지로 이동하시겠습니까?')
+})
+
+document.querySelector('.m_grade').addEventListener ('click',(e)=>{
+	alert('로그인이 필요한 서비스입니다.\n로그인페이지로 이동하시겠습니까?')
+})
+
+
+//-----상영 시간표 출력---------------------------------------------------------------------------------------------------//
+// 상영시간표 클릭 이벤트
+document.querySelector('.timetable_print').addEventListener ('click',(e)=>{
+	timetable_print()
+})
+
+function timetable_print(){
+	document.querySelector('.example_info').style.display='none';
+	document.querySelector('.time_table').style.display='flex';
+	list()
+}
+
+let 지역 = ``
+let 상영관 = ``
+let 상영시 = ``
+
+function local_print(){
+	
+	html =``
+	
+	spaceList.forEach((o,j)=>{
+		
+		html += `<button type="button" class="list" onclick="space_print(${j})"> ${o.local}</button>`
+		
+	})
+	
+	document.querySelector('.local_list').innerHTML = html
+	
+}	// fun e
+ 
+function space_print(j){
+	
+	if(document.querySelector('.date').value=='')
+	
+		{alert('관람일자를 설정해주세요.'); return ;}
+	
+	else{	html =``
+	
+			spaceList[j].space.forEach((o2,a)=>{
+		
+			html += `<button type="button" class="list" onclick="room_print(${a})"> ${o2.s}</button>`
+	
+			})
+			지역 = j
+			document.querySelector('.space_list').innerHTML = html } // if e
+
+}	// fun e
+ 
+function room_print(a){
+	
+	html =``
+	
+	roomList.forEach((o,k)=>{
+		
+		html += `<button type="button" class="list" onclick="time_print(${k})"> ${o.room}</button>`
+	
+	})
+	
+	document.querySelector('.space').innerHTML = `CGVOX `+spaceList[지역].space[a].s
+	document.querySelector('.room_list').innerHTML = html	
+	
+}
+ 
+function time_print(k){
+	
+	html =``
+	
+	roomList[k].times.forEach((o2,h)=>{
+		
+		html += `<button type="button" class="list" onclick="time_select(${h})"> ${o2.time}</button>`
+	
+	})
+	상영관 =k
+	document.querySelector('.room').innerHTML = roomList[k].room
+	document.querySelector('.mime_list').innerHTML = html
+}
+
+function time_select(h){
+	
+	day_select()	
+	document.querySelector('.tim').innerHTML = roomList[상영관].times[h].time	
+	상영시 = roomList[상영관].times[h].time	
+}
+
+function day_select(){
+	
+	document.querySelector('.day').innerHTML = document.querySelector('.date').value
+}
+
+
+//----에매 내역 모달 내용 출력----------------------------------------------------------------------------------------------//
+
+// 예매하기 버튼 클릭 이벤트
+ document.querySelector('.reserve').addEventListener('click',(e)=>{
+	 
+	 if(상영관==''){alert('상영관을 선택해 주세요.'); return;}
+	 
+	 else if(상영시==''){alert('관람시간을 선택해 주세요.'); return;}
+	 
+	 else{document.querySelector('.modal_wrap').style.display='flex';
+	 modal_print()}
+	 
+ })
+
+// 예매내역 
+function modal_print(){
+	
+	document.querySelector('.m_space').innerHTML	= document.querySelector('.space').innerHTML
+	document.querySelector('.m_movie').innerHTML	= document.querySelector('.m_title').innerHTML
+	document.querySelector('.m_room').innerHTML		= document.querySelector('.room').innerHTML
+	document.querySelector('.m_day').innerHTML		= document.querySelector('.day').innerHTML
+	document.querySelector('.m_time').innerHTML		= document.querySelector('.tim').innerHTML
+	document.querySelector('.m_price').innerHTML	= `16,000원`
+	
+}
+
+// 모달 버튼 이벤트
+ document.querySelector('.modal_cancel').addEventListener('click',(e)=>{
+	 
+	 document.querySelector('.modal_wrap').style.display='none';
+	 
+ })
+ 
---
+++ TeamProject1-CGV/public/js/store.js
@@ -0,0 +1,213 @@
+let categoryList = [  '콤보' , '팝콘' , '음료' , '스낵' , '영화관람권' , '기프트카드' , '지점한정메뉴' ]
+let productList = [
+	{ category: '콤보' , name : '더블콤보' , text : `더블콤보텍스트` , price : 11000 , img : 'img/store/더블콤보.png' },
+	{ category: '콤보' , name : '러브콤보' , text : `스위트콤보텍스트` , price : 12000 , img : 'img/store/러브콤보.png' },
+	{ category: '콤보' , name : '핫도그콤보' , text : `스몰세트텍스트` , price : 13000 , img : 'img/store/핫도그콤보.png'	},
+	{ category: '콤보' , name : '나쵸콤보' , text : `CGV콤보텍스트` , price : 14000 , img : 'img/store/나쵸콤보.png' } ,
+	{ category: '콤보' , name : '맛밤콤보' , text : `CGV콤보텍스트` , price : 14000 , img : 'img/store/맛밤콤보.png' }, 
+	{ category: '스낵' , name : '핫도그' , text : `핫도그텍스트` , price : 7000 , img : 'img/store/핫도그.png' } ,
+	{ category: '스낵' , name : '나쵸' , text : `나쵸텍스트` , price : 7000 , img : 'img/store/나쵸.png' } ,
+	{ category: '스낵' , name : '맛밤' , text : `맛밤텍스트` , price : 7000 , img : 'img/store/맛밤.png' },
+	{ category: '팝콘' , name : '팝콘M' , text : `팝콘M텍스트` , price : 8000 , img : 'img/store/팝콘M.png' },
+	{ category: '팝콘' , name : '팝콘L' , text : `팝콘L텍스트` , price : 9000 , img : 'img/store/팝콘L.png' },
+	{ category: '음료' , name : '콜라' , text : `콜라텍스트` , price : 9000 , img : 'img/store/콜라.png' } ,
+	{ category: '음료' , name : '아메리카노' , text : `아메리카노텍스트` , price : 4000 , img : 'img/store/아메리카노.png' },
+	{ category: '영화관람권' , name : '영화관람권' , text : `영화관람권텍스트` , price : 14000 , img : 'img/store/영화관람권.png' },
+	{ category: '기프트카드' , name : '기프트카드1' , text : `기프트카드1텍스트` , price : 14000 , img : 'img/store/기프트카드.png' },
+	{ category: '기프트카드' , name : '기프트카드2' , text : `기프트카드2텍스트` , price : 14000 , img : 'img/store/기프트카드2.png' },
+	{ category: '지점한정메뉴' , name : '[서울지점전용]라면팩' , text : `[서울지점전용]라면팩 텍스트` , price : 14000 , img : 'img/store/서울지점전용라면팩.png' },
+	{ category: '지점한정메뉴' , name : '[서울지점전용]짜장라면팩' , text : `[서울지점전용]짜장라면팩 텍스트` , price : 14000 , img : 'img/store/서울지점전용짜장라면팩.png' }
+]
+// 객체배열
+let cartListArray = []; let orderListArray = [];
+//전역변수
+let totalPrice = 0;
+//객체화
+let modal_wrap =  document.querySelector('.modal_wrap') //모달배경div
+//js실행시 함수실행
+category_print(); product_print(0);
+
+function category_print(){ // 카테고리 출력 함수
+	// html 선언
+	html = ''
+		// 객체의 카테고리 하나씩 꺼내기
+	for ( let i = 0 ; i < categoryList.length ; i++ ){
+		html += `
+			<li class="category_li" onclick="product_print(${i})">${categoryList[i]}</li>
+			`
+		console.log( " i  : " + i )
+	}
+	// 카테고리 html출력
+	document.querySelector('.categoryUl').innerHTML = html;
+	
+	
+}
+
+function product_print(i){ // 카테고리별 제품출력 함수
+	console.log( "categoryList 인덱스 : " + i )
+	
+	//카테고리 전부 가져오기 돔객체화 
+	let categoryLi = document.querySelectorAll('.category_li')
+	console.log( categoryLi[i] )
+	
+	for ( let c = 0 ; c < categoryLi.length ; c++ ){
+		if ( c == i ){
+			categoryLi[c].classList.add('categoryliPoint');
+		} else {
+			categoryLi[c].classList.remove('categoryliPoint');
+		}
+	}
+	
+	
+	// html 선언
+	html = '';
+	html2 = ``;
+	html2 = categoryList[i];
+	document.querySelector('.p_head').innerHTML = html2;
+	// 객체 하나씩 꺼내기
+	for ( let j = 0 ; j < productList.length ; j++ ){
+			// j는 0부터 productList 마지막 인덱스번호까지
+		if ( productList[j].category == categoryList[i]  ){
+		// html 추가
+		html += `
+			<div class="product" onclick="cartList_print(${j})">
+				<img class="product_img" alt="" src="${productList[j].img}">
+				<div class="product_title">${productList[j].name}</div>
+				<div class="product_text">${productList[j].text}</div>
+				<div class="product_price">${productList[j].price.toLocaleString()}원</div>
+			</div>
+			`
+		} 
+	}
+	// 카테고리 html출력
+	document.querySelector('.productDiv').innerHTML = html;
+}
+
+// 제품을 클릭하면 장바구니에 담기는 함수
+function cartList_print(j){
+	//장바구니에 제품 담기
+	let html = '';
+	console.log( ' cartList_print 함수 실행')
+	html += `
+			<div class="basket_product">
+				<img class="imgDiv" alt="" src="${productList[j].img}">
+				<div class="basket_pinfo">
+					<div class="binfo_title">${productList[j].name}</div>
+					<div class="binfo_price">${productList[j].price.toLocaleString()}원</div>
+				</div>
+			</div>
+			`
+	document.querySelector('.basket_bottom').innerHTML += html;
+	cartListArray.push(productList[j])
+	
+	// 장바구니 추가한 제품 개수 출력
+	// 총 금액 출력
+	cartCount_Total();
+}
+
+// 장바구니 추가한 제품 개수 출력
+// 총 금액 출력
+function cartCount_Total(){
+	
+	let basketNum = 0;
+	totalPrice = 0;
+	for ( let c = 0 ; c < cartListArray.length ; c++ ){
+		basketNum++
+		let price = cartListArray[c].price;
+		totalPrice += price;
+	}
+	document.querySelector('.basketNum').innerHTML = basketNum;
+	document.querySelector('.totalPrice').innerHTML = totalPrice.toLocaleString()+" 원";
+}
+
+//결제하기 버튼 누르면 [ 1.orderList에 cartList배열 push / 2. 결제정보 모달 출력 / 
+function order(){
+	
+	// 유효성검사
+	if ( cartListArray.length < 1 ){ alert("장바구니가 비어있습니다."); return; }
+	
+	// 1.orderList에 cartList객체배열 push
+	let order = {
+		time : new Date() ,
+		products : cartListArray ,
+		total : totalPrice.toLocaleString()
+	}
+	orderListArray.push(order)
+	console.log("orderListArray")
+	console.log(orderListArray)
+	
+	// 2. 숨겼던 모달 나타나기
+	modal_wrap.style.display = 'flex'
+	
+	// 3. 모달에 orderList내의 결제내역 출력
+	let html = '';
+	html += `
+				<tr> <th>극장명</th> <td>CGVOX</td> </tr>
+			`	
+	html += `
+					<tr> <th class="tableLine2">제품명</th> <td class="tableLine2">
+				`
+	/*------------------------------------------------------------*/
+	// 제품이름,주문개수  html에 추가
+	
+	for ( let o = 0 ; o < productList.length ; o++ ){
+		if ( count(o) != 0 ){
+		html += productList[o].name + 'x' + count(o) + '개 <br>'
+		}
+	}
+	function count(o){
+		let count = 0;
+			for ( let i = 0 ; i < orderListArray[0].products.length ; i++){
+				if ( productList[o].name == orderListArray[0].products[i].name){
+					count++ 
+				}
+			}
+			return count;
+	}
+	
+	html += `</td> </tr>`
+	/*------------------------------------------------------------*/
+	//주문시간  html에 추가
+	new Date().getHours()
+	new Date().getMinutes()
+	new Date().getSeconds()
+	let year = orderListArray[0].time.getFullYear()
+	let month = orderListArray[0].time.getMonth()+1
+	let date = orderListArray[0].time.getDate()
+	let hours = orderListArray[0].time.getHours()
+	let minutes = orderListArray[0].time.getMinutes()
+	let seconds = orderListArray[0].time.getSeconds()
+		
+	html += `<tr> <th>결제시간</th> <td>`
+	html += year+"년"+month+"월"+date+"일 "+hours+"시"+minutes+"분"+seconds+"초"
+	html += `</td> </tr>`
+	/*------------------------------------------------------------*/
+	// 총금액 html에 추가
+	html += `<tr> <th>결제금액</th> <td>${orderListArray[0].total}원</td> </tr>`
+	/*------------------------------------------------------------*/
+	// html 출력
+	document.querySelector('.ordertable').innerHTML = html;
+	cartListArray = []
+	orderListArray = []
+	product_print(0);
+	
+	//장바구니 비우기
+	document.querySelector('.basket_bottom').innerHTML = ''
+	
+	// 장바구니 추가한 제품 개수 출력
+	// 장바구니 총 금액 출력
+	cartCount_Total();
+}
+
+//닫기 버튼 이벤트리스너
+let close = document.querySelector('.close')
+close.addEventListener('click',(e)=>{
+	console.log('클로즈이벤트리스너실행')
+	modal_wrap.style.display = 'none'
+})
+
+let cancel = document.querySelector('.cancel')
+cancel.addEventListener('click',(e)=>{
+   console.log('클로즈이벤트리스너실행')
+   modal_wrap.style.display = 'none'
+})
